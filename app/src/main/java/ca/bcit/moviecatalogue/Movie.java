a22dafd816aad256541291191447edcc1b548d4f
Yuni <yshuh0126@gmail.com>
2020-04-16 12:24:18 -0700

    Added initial design for main activity

--- app/src/main/java/ca/bcit/moviecatalogue/Movie.java
+++ app/src/main/java/ca/bcit/moviecatalogue/Movie.java
@@ -12,6 +12,30 @@ public class Movie {
         this.link = link;
     }
 
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
     @Override
     public String toString() {
         return "Movie{" +
